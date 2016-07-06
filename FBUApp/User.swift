932d1292729a2856a54cc25832e6c3e5df5d1582
Jedidiah Akano <jakano@fb.com>
2016-07-06 10:43:34 -0700

    User Model Created

---
+++ FBUApp/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  FBUApp
+//
+//  Created by Jedidiah Akano on 7/6/16.
+//  Copyright © 2016 Kelly Lampotang. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
