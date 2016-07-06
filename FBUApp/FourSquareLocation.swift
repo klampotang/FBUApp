524c0262e232d3c2a35a1b37cac0f2b5c6fe35b0
Jedidiah Akano <jakano@fb.com>
2016-07-06 14:20:23 -0700

    Api progress

---
+++ FBUApp/FourSquareLocation.swift
@@ -0,0 +1,48 @@
+//
+//  FourSquareLocation.swift
+//  FBUApp
+//
+//  Created by Jedidiah Akano on 7/6/16.
+//  Copyright © 2016 Kelly Lampotang. All rights reserved.
+//
+
+import UIKit
+
+class FourSquareLocation: NSObject {
+    let CLIENT_ID = "VM3U2MOM4P4IOVBQMZW5NSQH1MJEANGW51YA4SVMW4JLAEBH"
+    let CLIENT_SECRET = "5FHY2RUJVEW2YRHJ3J53XN5J0U2HUY5KNQMFSCHJVMQPMDUC"
+    init() {
+        <#code#>
+    }
+    
+    
+    func fetchLocations(query: String, near: String = "San Francisco") {
+        let baseUrlString = "https://api.foursquare.com/v2/venues/search?"
+        let queryString = "client_id=\(CLIENT_ID)&client_secret=\(CLIENT_SECRET)&v=20141020&near=\(near),CA&query=\(query)"
+        
+        let url = NSURL(string: baseUrlString + queryString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!)!
+        let request = NSURLRequest(URL: url)
+        
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+                                                                      completionHandler: { (dataOrNil, response, error) in
+                                                                        if let data = dataOrNil {
+                                                                            if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                                                                                data, options:[]) as? NSDictionary {
+                                                                                NSLog("response: \(responseDictionary)")
+                                                                                self.results = responseDictionary.valueForKeyPath("response.venues") as! NSArray
+                                                                                self.tableView.reloadData()
+                                                                                
+                                                                            }
+                                                                        }
+        });
+        task.resume()
+
+
+}
