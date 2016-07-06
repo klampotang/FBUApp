286cc03140d8f1b2cdca9f28dfc4a23e0cb2e68b
Angeline Rao <angierao@fb.com>
2016-07-06 09:42:08 -0700

    more changes

--- FBUApp/MainView.swift
+++ FBUApp/MainView.swift
@@ -9,6 +9,52 @@
 import UIKit
 
 class MainView: UIView {
+    let imageMarginSpace: CGFloat = 3.0
+    var pictureView: UIImageView!
+    var originalCenter: CGPoint!
+    var animator: UIDynamicAnimator!
+    
+    init(frame: CGRect, center: CGPoint, image: UIImage) {
+        self.pictureView.image = image
+        super.init(frame: frame)
+        self.center = center
+        self.originalCenter = center
+        self.pictureView.frame = CGRectIntegral(CGRectMake(
+            0.0 + self.imageMarginSpace,
+            0.0 + self.imageMarginSpace,
+            self.frame.width - (2 * self.imageMarginSpace),
+            self.frame.height - (2 * self.imageMarginSpace)
+            ))
+        
+    self.addSubview(pictureView)
+
+    }
+    
+    func swipe(answer: Bool) {
+        animator.removeAllBehaviors()
+        
+        // If the answer is false, Move to the left
+        // Else if the answer is true, move to the right
+        let gravityX = answer ? 0.5 : -0.5
+        let magnitude = answer ? 20.0 : -20.0
+        let gravityBehavior:UIGravityBehavior = UIGravityBehavior(items: [self])
+        gravityBehavior.gravityDirection = CGVectorMake(CGFloat(gravityX), 0)
+        animator.addBehavior(gravityBehavior)
+        
+        let pushBehavior:UIPushBehavior = UIPushBehavior(items: [self], mode: UIPushBehaviorMode.Instantaneous)
+        pushBehavior.magnitude = CGFloat(magnitude)
+        animator.addBehavior(pushBehavior)
+        
+    }
+    
+    func returnToCenter() {
+        UIView.animateWithDuration(0.8, delay: 0.1, usingSpringWithDamping: 0.8, initialSpringVelocity: 1.0, options: .AllowUserInteraction, animations: {
+            self.center = self.originalCenter
+            }, completion: { finished in
+                print("Finished Animation")}
+        )
+        
+    }
 
     /*
     // Only override drawRect: if you perform custom drawing.
--- FBUApp/MainViewController.swift
+++ FBUApp/MainViewController.swift
@@ -9,12 +9,75 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    var images: [UIImage] = [UIImage(named: "bluebird"), UIImage(named: "favorite-action"), UIImage(named: "reply-action"), UIImage(named: "retweet-action")]
+    
+    var currentMainView: MainView!
+    var mainViews: [MainView] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        for image in images {
+            currentMainView = MainView(
+                frame: CGRectMake(0, 0, self.view.frame.width - 50, self.view.frame.width - 50),
+                center: CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 3),
+                image: image)
+            self.mainViews.append(currentMainView)
+        }
+        
+        for mainView in mainViews {
+            self.view.addSubview(mainView)
+        }
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        self.view.addGestureRecognizer(pan)
     }
+    
+    
+    func determineJudgement(answer: Bool) {
+        // Run the swipe animation
+        self.currentMainView.swipe(answer)
+        
+        // Handle when we have no more matches
+        self.mainViews.removeAtIndex(self.mainViews.count - 1)
+        if self.mainViews.count - 1 < 0 {
+            let noMoreView = MainView(
+                frame: CGRectMake(0, 0, self.view.frame.width - 50, self.view.frame.width - 50),
+                center: CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 3),
+                image: UIImage()
+            )
+            self.mainViews.append(noMoreView)
+            self.view.addSubview(noMoreView)
+            self.done = true
+            return
+        }
+        
+        // Set the new current question to the next one
+        self.currentMainView = self.mainViews.last!
+        
+    }
+    
+    func handlePan(gesture: UIPanGestureRecognizer) {
+        // Is this gesture state finished??
+        if gesture.state == UIGestureRecognizerState.Ended {
+            // Determine if we need to swipe off or return to center
+            let location = gesture.locationInView(self.view)
+            if self.currentMainView.center.x / self.view.bounds.maxX > 0.8 {
+                self.determineJudgement(true)
+            }
+            else if self.currentMainView.center.x / self.view.bounds.maxX < 0.2 {
+                self.determineJudgement(false)
+            }
+            else {
+                self.currentMainView.returnToCenter()
+            }
+        }
+        let translation = gesture.translationInView(self.currentMainView)
+        self.currentMainView.center = CGPoint(x: self.currentMainView!.center.x + translation.x, y: self.currentMainView!.center.y + translation.y)
+        gesture.setTranslation(CGPointZero, inView: self.view)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
