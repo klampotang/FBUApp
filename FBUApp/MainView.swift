6af58b2a48cf7cbb9e2ad665c195b1c6a95d6d28
Angeline Rao <angierao@fb.com>
2016-07-06 08:30:30 -0700

    changes

---
+++ FBUApp/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  FBUApp
+//
+//  Created by Angeline Rao on 7/6/16.
+//  Copyright © 2016 Kelly Lampotang. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
