c54f14f73344578b9f8c6a015491972c3c41c7a0
Jedidiah Akano <jakano@fb.com>
2016-07-06 15:28:32 -0700

    Location work

--- FBUApp/FourSquareLocation.swift
+++ FBUApp/FourSquareLocation.swift
@@ -6,11 +6,14 @@
 //  Copyright © 2016 Kelly Lampotang. All rights reserved.
 //
 
-/*import UIKit
+import UIKit
 
 class FourSquareLocation: NSObject {
+    var results: NSArray = []
+    var location: String?
     let CLIENT_ID = "VM3U2MOM4P4IOVBQMZW5NSQH1MJEANGW51YA4SVMW4JLAEBH"
     let CLIENT_SECRET = "5FHY2RUJVEW2YRHJ3J53XN5J0U2HUY5KNQMFSCHJVMQPMDUC"
+    
     
     func fetchLocations(query: String, near: String = "San Francisco") {
         let baseUrlString = "https://api.foursquare.com/v2/venues/search?"
@@ -26,19 +29,16 @@ class FourSquareLocation: NSObject {
         )
         
         
-        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
-                                                                      completionHandler: { (dataOrNil, response, error) in
-                                                                        if let data = dataOrNil {
-                                                                            if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-                                                                                data, options:[]) as? NSDictionary {
-                                                                                NSLog("response: \(responseDictionary)")
-                                                                                self.results = responseDictionary.valueForKeyPath("response.venues") as! NSArray
-                                                                                self.tableView.reloadData()
-                                                                                
-                                                                            }
-                                                                        }
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: { (dataOrNil, response, error) in
+            if let data = dataOrNil {
+                if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(data, options:[]) as? NSDictionary {
+                    NSLog("response: \(responseDictionary)")
+                    self.results = responseDictionary.valueForKeyPath("response.venues") as! NSArray
+                }
+            }
         });
         task.resume()
 
 
-}*/
+        }
+}
