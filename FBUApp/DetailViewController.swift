2921775057cc7d3f6c5e138fa1a98023d0737d0c
Jedidiah Akano <jakano@fb.com>
2016-07-07 11:37:59 -0700

    Switching to fabricator

--- FBUApp/DetailViewController.swift
+++ FBUApp/DetailViewController.swift
@@ -24,12 +24,14 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var cardPicture: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.distanceLabel.text = String(card!["distance"])
 
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
     
